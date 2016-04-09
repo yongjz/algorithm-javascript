c8f96073eaa32253bf897ace6e91453c10caeff5
yongjz <yjz_1008@163.com>
2016-04-09 17:21:21 +0800

    add binary search func

---
+++ src/search/binarySearch.js
@@ -0,0 +1,31 @@
+'use strict';
+
+//数组arr必须是有序的
+const binarySearch = (key, arr) => {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    let mid = Math.floor(low + (high - low) / 2);
+    if(key < arr[mid])
+      high = mid - 1;
+    else if(key > arr[mid])
+      low = mid + 1;
+    else
+      return mid;
+  }
+  return -1;
+}
+
+//测试
+const main = (argv) => {
+  console.log('index:' + binarySearch(9, [1,3,4,6,8,9,9,16,17,28]));
+}
+
+if (require.main === module) {
+  // 如果是直接执行 main.js，则进入此处
+  // 如果 main.js 被其他文件 require，则此处不会执行。
+  main();
+}
+
+exports.binarySearch = binarySearch;
+
