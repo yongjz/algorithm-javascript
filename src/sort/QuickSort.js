e12619655026da47f16a885c0441913e920e7f40
yongjz <yjz_1008@163.com>
2016-04-13 18:19:20 +0800

    add quick sort

---
+++ src/sort/QuickSort.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const SortBase = require('./SortBase');
+
+class QuickSort extends SortBase {
+  constructor() {
+    super();
+  }
+
+  //切分方法
+  partition(arr, low, high) {
+    let i = low, j = high + 1;
+    let v = arr[low];
+    while (true) {
+      while (this.less(arr[++i], v)) {
+        if (i === high) break;
+      }
+      while (this.less(v, arr[--j])) {
+        if (j === low) break;
+      }
+      if (i >= j) break;
+      this.exch(arr, i, j);
+    }
+    this.exch(arr, low, j);
+    return j;
+  }
+
+  // 使用递归, 自顶向下排序
+  sort(arr) {
+    this.quickSort(arr, 0, arr.length - 1);
+  }
+
+  quickSort(arr, low, high) {
+    if (high <= low)
+      return;
+    let j = this.partition(arr, low, high);
+    console.log('j='+j);
+    this.quickSort(arr, low, j - 1);
+    this.quickSort(arr, j + 1, high);
+  }
+}
+
+const main = () => {
+  //let arr = [7, 3, 12, 1, 5, 10, 23, 30, 6];
+  let arr = ['S', 'O', 'R', 'T', 'E', 'X', 'A', 'M', 'P', 'L', 'E'];
+  let s = new QuickSort();
+  console.log(s.isSorted(arr));
+  s.sort(arr);
+  s.show(arr);
+  console.log(s.isSorted(arr));
+}
+
+if (require.main === module) {
+  main();
+}
+
+module.exports = QuickSort;
+
