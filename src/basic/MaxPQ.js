3ab99920539fc0e648e814952ac2b507cf35d818
yongjz <yjz_1008@163.com>
2016-04-15 23:47:18 +0800

    add priority queue

--- src/basic/MaxPQ.js
+++ src/basic/MaxPQ.js
@@ -7,15 +7,20 @@ class MaxPQ {
   }
 
   insert(item) {
-    
+    this.pq[++this.N] = item;
+    this.swim(this.N);
   }
 
   max() {
-
+    return this.pq[1];
   }
 
   delMax() {
-
+    const max = this.pq[1];
+    this.exch(1, this.N--);
+    this.pq[this.N + 1] = null;
+    this.sink(1);
+    return max;
   }
 
   isEmpty() {
@@ -27,7 +32,7 @@ class MaxPQ {
   }
 
   less(i, j) {
-    return i < j;
+    return this.pq[i] < this.pq[j];
   }
 
   exch(i, j) {
@@ -37,15 +42,44 @@ class MaxPQ {
   }
 
   swim(k) {
-    while (k > 1 && less(Math.floor(k / 2), k)) {
+    while (k > 1 && this.less(Math.floor(k / 2), k)) {
       this.exch(Math.floor(k / 2), k);
       k = Math.floor(k / 2);
     }
   }
 
   sink(k) {
-    
+    while (2 * k <= this.N) {
+      let j = 2 * k;
+      if(j < this.N && this.less(j, j+1)) j++;
+      if(!this.less(k, j)) break;
+      this.exch(k, j);
+      k = j;
+    }
   }
+}
+
+const main = () => {
+  var m = new MaxPQ(0);
+  m.insert(10);
+  m.insert(8);
+  m.insert(9);
+  m.insert(15);
+  m.insert(6);
+  m.insert(20);
+  m.insert(1);
+  m.insert(3);
+  m.insert(30);
+  m.insert(33);
+  console.log(m.pq);
+
+  for(let i = 0;i < 8; i++) {
+    console.log(m.delMax());
+  }
+}
+
+if (require.main === module) {
+  main();
 }
 
 module.exports = MaxPQ;
